d07c83777fc7cebd439f585cdb1e4f10f38b9aaf
+ <+>
2022-08-24 11:12:29 -0400

    added to EditProfile

--- EditProfile.js
+++ EditProfile.js
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import { useState, useEffect } from "react";
 import { Image, Text, View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+import ChatListItem from './ChatListItem';
 
 function EditProfile() {
     return (
-        <View>
-        <Text>Here we edit profile pic and status</Text>
-        <TouchableOpacity style={deleteStyles.container}>
-            <Text style={deleteStyles.text}>
+        <View style={profileStyles.profileContainer}>
+        <View style={profileStyles.profileCardContainer}>
+        <ChatListItem />
+
+        <View style={profileStyles.statusContainer}>
+        <Text style={profileStyles.inputlabel}>Edit Status</Text>
+        <TextInput style={profileStyles.input}></TextInput>
+        </View>
+
+        </View>
+        
+        <TouchableOpacity style={profileStyles.deleteContainer}>
+            <Text style={profileStyles.deleteText}>
                 Delete account
             </Text>
         </TouchableOpacity>
@@ -15,20 +25,49 @@ function EditProfile() {
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
+
+const profileStyles = StyleSheet.create({
+    profileContainer: {
         flex: 1,
         width: "100%",
-        height: "100%"
-    }
+        height: "100%",
+        position: "absolute",
+        backgroundColor: "whitesmoke",
+        justifyContent: "space-evenly",
+        alignItems: "center"
 
-})
+    },
+    profileCardContainer: {
+        display: "flex",
+        height: 300,
+        width: "100%",
+        backgroundColor: "white",
+        justifyContent: "space-evenly",
+        alignItems: "center"
 
-const deleteStyles = StyleSheet.create({
-    text: {
+    },
+    statusContainer: {
+        display: "flex",
+        // backgroundColor: "red",
+        width: 320
+
+    },
+    input: {
+        backgroundColor: "whitesmoke",
+        height: 50,
+        width: "100%",
+        borderRadius: 10
+    },
+    inputlabel: {
+        paddingLeft: 5,
+        paddingBottom: 5
+    },
+
+    deleteText: { 
         color: "red"
     },
-    container: {
+    
+    deleteContainer: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
